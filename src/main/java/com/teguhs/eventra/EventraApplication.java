4da47cfb3ab74b8c538407723f851a22973b09db
Muhammad Teguh Sinulingga <teguhmuhams@gmail.com>
2024-09-30 16:00:01 +0700

    Initial commit

---
+++ src/main/java/com/teguhs/eventra/EventraApplication.java
@@ -0,0 +1,13 @@
+package com.teguhs.eventra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventraApplication.class, args);
+	}
+
+}
